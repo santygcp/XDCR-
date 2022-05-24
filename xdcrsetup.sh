f438daa50885f63bbf579dc62d06bef8a0dd2a37
santygcp <97255596+santygcp@users.noreply.github.com>
2022-05-24 18:11:44 +0530

    Update xdcrsetup.sh

--- xdcrsetup.sh
+++ xdcrsetup.sh
@@ -4,17 +4,17 @@ DOCKER_ID="jadejakajal13"
 DOCKER_API="b461d1b4-82c4-499e-afc0-f17943a16411"
 DOCKER_EMAIL="jadejakajal13@gmail.com"
 LICENSE_FILE="/opt/voltdb/voltdb/license.xml"
-XDCR="XDCR.yaml"
+XDCR="XDCR2.yaml"
 
 
 helm repo add santy https://voltdb-kubernetes-charts.storage.googleapis.com
 
 #creating a cluster
 
-gcloud beta container --project santosh-350416 clusters create xdcr1 --zone us-east4-a --no-enable-basic-auth --cluster-version "1.22.6-gke.300" --machine-type "n2-standard-2" --disk-type "pd-standard" --disk-size "100" --num-nodes "3"
+gcloud beta container --project santosh-350416 clusters create xdcr --zone us-east4-a --no-enable-basic-auth --cluster-version "1.22.6-gke.300" --machine-type "n2-standard-2" --disk-type "pd-standard" --disk-size "100" --num-nodes "3"
 
 #connecting to the cluster
-gcloud container clusters get-credentials xdcr1 --zone us-east4-a --project santosh-350416
+gcloud container clusters get-credentials xdcr --zone us-east4-a --project santosh-350416
 
 #create the secret registry
 kubectl create secret docker-registry dockerio-registry --docker-username=$DOCKER_ID --docker-email=$DOCKER_EMAIL --docker-password=$DOCKER_API
