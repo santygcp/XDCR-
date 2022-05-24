14c173ccd2352b5b36442cfb0d159d55dfee3dc7
santygcp <97255596+santygcp@users.noreply.github.com>
2022-05-24 19:57:11 +0530

    Update xdcrsetup.sh

--- xdcrsetup.sh
+++ xdcrsetup.sh
@@ -37,7 +37,7 @@ kubectl get all | grep LoadBalancer | sed -n '1,1p' |awk '{ print $4 }'
 #working
 #kubectl cp voter/voter-procs.jar  xdcr1-voltdb-cluster-0:/tmp/ 
 #kubectl exec -it xdcr1-voltdb-cluster-0 -- sqlcmd < voter/ddl.sql
-kubectl exec -it xdcr1-voltdb-cluster-0 -- run.sh client
+kubectl exec -it xdcr1-voltdb-cluster-0 -- voter/run.sh client
 
 
 
