a737da484d8cccbea36c663af9b961ee21c8dcc7
santygcp <97255596+santygcp@users.noreply.github.com>
2022-05-24 19:10:54 +0530

    Update xdcrsetup.sh

--- xdcrsetup.sh
+++ xdcrsetup.sh
@@ -7,32 +7,34 @@ LICENSE_FILE="/opt/voltdb/voltdb/license.xml"
 XDCR="XDCR2.yaml"
 
 
-helm repo add santy https://voltdb-kubernetes-charts.storage.googleapis.com
+#helm repo add santy https://voltdb-kubernetes-charts.storage.googleapis.com
 
 #creating a cluster
 
-gcloud beta container --project santosh-350416 clusters create xdcr --zone us-central1-a --no-enable-basic-auth --cluster-version "1.22.6-gke.300" --machine-type "n2-standard-2" --disk-type "pd-standard" --disk-size "100" --num-nodes "3"
+#gcloud beta container --project santosh-350416 clusters create xdcr --zone us-central1-a --no-enable-basic-auth --cluster-version "1.22.6-gke.300" --machine-type "n2-standard-2" --disk-type "pd-standard" --disk-size "100" --num-nodes "3"
 
 #connecting to the cluster
-gcloud container clusters get-credentials xdcr --zone us-central1-a --project santosh-350416
+#gcloud container clusters get-credentials xdcr --zone us-central1-a --project santosh-350416
 
 #create the secret registry
-kubectl create secret docker-registry dockerio-registry --docker-username=$DOCKER_ID --docker-email=$DOCKER_EMAIL --docker-password=$DOCKER_API
+#kubectl create secret docker-registry dockerio-registry --docker-username=$DOCKER_ID --docker-email=$DOCKER_EMAIL --docker-password=$DOCKER_API
 
 
-helm install xdcr1 santy/voltdb --set cluster.clusterSpec.replicas=3 --set cluster.config.deployment.cluster.kfactor=1 --set-file cluster.config.licenseXMLFile=$LICENSE_FILE  -f $XDCR
+#helm install xdcr1 santy/voltdb --set cluster.clusterSpec.replicas=3 --set cluster.config.deployment.cluster.kfactor=1 --set-file cluster.config.licenseXMLFile=$LICENSE_FILE  -f $XDCR
 
-sleep 360
+#sleep 360
 
 #kubectl port-forward xdcr1-voltdb-cluster-0 8080 21212
 
 kubectl get nodes -o wide | tail -1 | awk -F " " {'print $7'}
-echo "VolTB Port for UI access"
+#echo "VolTB Port for UI access"
 
 kubectl get svc  | grep http |awk -F " " {'print $5'}
-echo "grafana Port for UI access"
+#echo "grafana Port for UI access"
 
 kubectl get all | grep LoadBalancer | sed -n '1,1p' |awk '{ print $4 }' 
+
+
 
 
 
