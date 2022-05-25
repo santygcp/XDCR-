f0d3e17f36958258d4034050f8dc1fb818877dda
santygcp <97255596+santygcp@users.noreply.github.com>
2022-05-25 12:50:04 +0530

    Update xdcrsetup.sh

--- xdcrsetup.sh
+++ xdcrsetup.sh
@@ -40,9 +40,9 @@ XDCR="XDCR2.yaml"
 
 #working
 
-kubectl cp voter/voter-client.jar  xdcr1-voltdb-cluster-0:/tmp/ 
-kubectl cp voter/voter-procs.jar  xdcr1-voltdb-cluster-0:/tmp/ 
-kubectl cp voter/run.sh  xdcr1-voltdb-cluster-0:/tmp/
+kubectl cp voter/*/  xdcr1-voltdb-cluster-0:/tmp/ 
+#kubectl cp voter/voter-procs.jar  xdcr1-voltdb-cluster-0:/tmp/ 
+#kubectl cp voter/run.sh  xdcr1-voltdb-cluster-0:/tmp/
 kubectl exec -it xdcr1-voltdb-cluster-0 -- sqlcmd < voter/ddl.sql
 kubectl exec -it xdcr1-voltdb-cluster-0 -- /bin/bash -c "/tmp/run.sh init"
 kubectl exec -it xdcr1-voltdb-cluster-0 -- /bin/bash -c "/tmp/run.sh client"
