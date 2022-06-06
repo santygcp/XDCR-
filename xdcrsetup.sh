ca02f4ffe07b4fb971d72f55536c49c1611af411
santygcp <97255596+santygcp@users.noreply.github.com>
2022-06-06 14:17:37 +0530

    Update xdcrsetup.sh

--- xdcrsetup.sh
+++ xdcrsetup.sh
@@ -53,6 +53,29 @@ if [ $retVal -ne 0 ]; then
     echo "all job's completed"
 fi
 
+gcloud beta container --project santosh-350416  clusters create xdcr2 --zone us-west1-a --no-enable-basic-auth --cluster-version "1.22.8-gke.201" --machine-type "n2-standard-2" --num-nodes 5 --disk-type "pd-standard" --disk-size "100"
+
+gcloud container clusters get-credentials xdcr2 --zone us-west1-a --project santosh-350416
+
+DOCKER_ID="jadejakajal13"
+DOCKER_API="b461d1b4-82c4-499e-afc0-f17943a16411"
+DOCKER_EMAIL="jadejakajal13@gmail.com"
+LICENSE_FILE="license.xml"
+XDCR="XDCR.yaml"
+
+kubectl create secret docker-registry dockerio-registry --docker-username=$DOCKER_ID --docker-email=$DOCKER_EMAIL --docker-password=$DOCKER_API
+
+helm install xdcr2 santy/voltdb --set cluster.clusterSpec.replicas=3 --set cluster.config.deployment.cluster.kfactor=1 --set-file cluster.config.licenseXMLFile=$LICENSE_FILE  -f $XDCR
+
+sleep 360
+
+kubectl cp run.sh votertestfinal:/opt/voltdb/voter/run.sh/
+
+sleep 180
+
+kubectl exec -it votertestfinal -- /bin/bash -c "cd /opt/voltdb/voter/ ; ./run.sh init xdcr1-voltdb-cluster-client.default.svc.cluster.local"
+
+
 
 
 
