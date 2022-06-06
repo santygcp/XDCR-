017937e6c7ab8e7a6abba2573ee1f22aebab8e9d
santygcp <97255596+santygcp@users.noreply.github.com>
2022-06-06 19:57:47 +0530

    Update xdcrsetup.sh

--- xdcrsetup.sh
+++ xdcrsetup.sh
@@ -11,10 +11,10 @@ helm repo add santy https://voltdb-kubernetes-charts.storage.googleapis.com
 
 #creating a cluster
 
-gcloud beta container --project santosh-350416  clusters create xdcr1 --zone us-west1-a --no-enable-basic-auth --cluster-version "1.22.8-gke.201" --machine-type "n2-standard-2" --num-nodes 5 --disk-type "pd-standard" --disk-size "100"
+gcloud beta container --project fourth-epigram-293718  clusters create xdcr1 --zone us-central1-a --no-enable-basic-auth --cluster-version "1.22.8-gke.201" --machine-type "n2-standard-2" --num-nodes 5 --disk-type "pd-standard" --disk-size "100"
 
 #connecting to the cluster
-gcloud container clusters get-credentials xdcr1 --zone us-west1-a --project santosh-350416
+gcloud container clusters get-credentials xdcr1 --zone us-central1-a --project fourth-epigram-293718
 
 #create the secret registry
 kubectl create secret docker-registry dockerio-registry --docker-username=$DOCKER_ID --docker-email=$DOCKER_EMAIL --docker-password=$DOCKER_API
@@ -54,15 +54,17 @@ if [ $retVal -ne 0 ]; then
     echo "all job's completed"
 fi
 
-gcloud beta container --project santosh-350416  clusters create xdcr2 --zone us-west1-a --no-enable-basic-auth --cluster-version "1.22.8-gke.201" --machine-type "n2-standard-2" --num-nodes 5 --disk-type "pd-standard" --disk-size "100"
-
-gcloud container clusters get-credentials xdcr2 --zone us-west1-a --project santosh-350416
-
 DOCKER_ID="jadejakajal13"
 DOCKER_API="b461d1b4-82c4-499e-afc0-f17943a16411"
 DOCKER_EMAIL="jadejakajal13@gmail.com"
 LICENSE_FILE="license.xml"
 XDCR="XDCR.yaml"
+
+gcloud beta container --project fourth-epigram-293718  clusters create xdcr2 --zone us-central1-a --no-enable-basic-auth --cluster-version "1.22.8-gke.201" --machine-type "n2-standard-2" --num-nodes 5 --disk-type "pd-standard" --disk-size "100"
+
+gcloud container clusters get-credentials xdcr2 --zone us-central1-a --project fourth-epigram-293718
+
+
 
 kubectl create secret docker-registry dockerio-registry --docker-username=$DOCKER_ID --docker-email=$DOCKER_EMAIL --docker-password=$DOCKER_API
 
@@ -94,14 +96,14 @@ echo "VolTB Port for UI access"
 kubectl get svc  | grep http |awk -F " " {'print $5'}
 
 
-gcloud container clusters get-credentials xdcr1 --zone us-west1-a --project santosh-350416
+gcloud container clusters get-credentials xdcr1 --zone us-central1-a --project fourth-epigram-293718
 
 N=kubectl get all | grep LoadBalancer | sed -n '1,1p' |awk '{ print $4 }' 
 
 helm upgrade xdcr1 santy/voltdb --reuse-values --set cluster.config.deployment.dr.connection.source=$N
 
 
-gcloud container clusters get-credentials xdcr2 --zone us-west1-a --project santosh-350416
+gcloud container clusters get-credentials xdcr2 --zone us-central1-a --project fourth-epigram-293718
 
 
 L=kubectl get all | grep LoadBalancer | sed -n '1,1p' |awk '{ print $4 }' 
